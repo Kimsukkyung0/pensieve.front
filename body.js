663769f0578057d1d804454bfb57ff044cb087e0
Min kyung suk <smk93021@gmail.com>
2023-11-09 11:25:27 +0900

    first commit

---
+++ body.js
@@ -0,0 +1,20 @@
+var msg = document.querySelector
+
+$(document).ready(function(){
+	$.ajax({
+		type: application/json,
+        charset : UTF-8,
+		url : `http://localhost:8085/post`,
+		data: ,
+		dataType: application/json , 
+        charset : UTF-8,
+		success: function(result){
+			alert(data); 
+		},
+		error:function(){  
+            alert('실패');
+		}
+	})
+});
+
+Access-Control-Allow-origin : 
