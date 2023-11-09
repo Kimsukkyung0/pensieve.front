553fac39ac99010c45ab2fbeddf1a9a22c0d65e4
Min kyung suk <smk93021@gmail.com>
2023-11-09 14:59:30 +0900

    405 error?

--- setting/body.js
+++ setting/body.js
@@ -1,24 +1,46 @@
 window.onload = function(){
+	
+
 	console.log("[window.onload] : [start]" );
 	console.log(location.origin);
+	var jsonData = document.getElementById("ctnt");
+	// const Http = new XMLHttpRequest();
+
 		$(document).ready(function(){
+			// const Http = new XMLHttpRequest();
+			const Url = 'http://localhost:8085/post';
+			
 			$('.send').click(function(){ 
-				let sendData = 
-				// let sendDataTest = "ctnt="+$('input[ctnt]').val();  
-				console.log(sendData);
-				$.ajax({
-					type:'post',  
-					url:'http://localhost:8085', 
-					data:sendData, 
-					dataType:'json',
-					success : function(data){
-						$('.result').html(data); 
-					},
-					error : function(data){
-						$('.result').html("failed");
+				if(jsonData!=null){
+					// Http.open("post",Url);
+					// Http.send();
+					// Http.onreadystatechange=(e)=>{
+					var sendJsonData = {
+						"userId" : 2,
+						"ctnt" : jsonData.value
 					}
-				});
+					// let sendDataTest = "ctnt="+$('input[ctnt]').val();  
+					console.log(sendJsonData);
+					$.ajax({
+						type:"POST",  
+						url : URL, 
+						async:true,
+						data:sendJsonData, 
+						contentType: "application/json; charset=utf-8",
+						dataType:"JSON",
+						success : function(data){
+							$('.result').html(JSON.stringify(data)); 
+							
+						},
+						error : function(data){
+							$('.result').html("failed");
+						}
+					
+					});
+				
+				}else{console.log("유효한 값이 아닙니다");}
 			});
+			
 		});
 
 }
