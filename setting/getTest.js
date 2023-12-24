60669efbec456c70a4a274b7df1aa7bfd76a6c86
Min kyung suk <smk93021@gmail.com>
2023-12-24 15:59:24 +0900

    getmethod된다

--- setting/getTest.js
+++ setting/getTest.js
@@ -1,16 +1,24 @@
 
+var xhr = new XMLHttpRequest();
 
 var fnGet = function() {
-    var xhr = new XMLHttpRequest();
+    
     xhr.open('GET', 'http://localhost:8085/list');
-    if (xhr.status === 200) {
+    xhr.onload = () => {
+    if (xhr.status === 200 || xhr.status === 201) {
         alert('요청성공 , 리스트 정보' + xhr.responseText);
+        
     }
     else {
         alert('요청실패 , 상태 : ' + xhr.status);
     }
+}
+    xhr.abort();
     xhr.send();
 };
 
+// var showDate = function(()=>{
+
+// });
 
 
