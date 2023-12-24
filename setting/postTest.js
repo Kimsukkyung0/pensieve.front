4e1b9858b6d1e49fe2dbe6a163ec71fd72c3c0f7
Min kyung suk <smk93021@gmail.com>
2023-12-24 16:40:17 +0900

    postTest 415에러 확인중

--- setting/postTest.js
+++ setting/postTest.js
@@ -1,14 +1,21 @@
-	// console.log("[window.onload] : [start]" );
-	
-	
-	
-	
-	//헤더 값 중 content-type 정의
-
 	const fnPost = function() {
+		if(confirm("요청을 전송하시겠습니까?")){
+			//confirm시, 하단 코드 실행
 		console.log("location.origin : " + location.origin);
 		var xhr = new XMLHttpRequest();
-		xhr.open('POST', 'http://localhost:8085/post');
+
+		xhr.onload = () => {
+			if (xhr.status === 200) {
+				alert('요청성공 , 리스트 정보' + xhr.responseText);
+			}
+			else {
+				alert('요청실패 , 상태 : ' + xhr.status);
+			}
+	
+		}
+
+
+		xhr.open('POST', 'http://localhost:8085/post');				
 		xhr.setRequestHeader("content-type", "application/json"); 
 		
 		var targetData = document.getElementById("ctnt");
@@ -16,51 +23,9 @@
 		var postTestData = {
 			"userId" : 2,
 			"ctnt" : targetData.value
-		}
-		if (xhr.status === 200) {
-			alert('요청성공 , 리스트 정보' + xhr.responseText);
-		}
-		else {
-			alert('요청실패 , 상태 : ' + xhr.status);
-		}
-
+		};
 		xhr.send(JSON.stringify(postTestData));
+		
+		}
 	};
 	
-	
-
-
-
-		// $(document).ready(function(){
-		// 	// const Http = new XMLHttpRequest();
-		// 	const Url = 'http://localhost:8085/post';
-			
-		// 	$('.send').click(function(){ 
-		// 		if(jsonData!=null){
-
-		// 			var sendJsonData = {
-		// 				"userId" : 2,
-		// 				"ctnt" : jsonData.value
-		// 			}
-		// 			console.log(sendJsonData);
-		// 			$.ajax({
-		// 				type:"GET",  
-		// 				url : URL, 
-		// 				async:true,
-		// 				data:sendJsonData, 
-		// 				contentType: "application/json; charset=utf-8",
-		// 				dataType:"JSON",
-		// 				success : function(data){
-		// 					$('.result').html(JSON.stringify(data)); 
-							
-		// 				},
-		// 				error : function(data){
-		// 					$('.result').html("failed");
-		// 				}
-					
-		// 			});
-				
-		// 		}else{console.log("유효한 값이 아닙니다");}
-		// 	});
-			
-		// });
